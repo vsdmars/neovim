8c4b7e9c41c2376bf2be4d815e7a2f80fbe43b85
vsdmars <vsdmars@gmail.com>
2023-03-05 06:27:35 -0800

    update

--- lua/00_lazy.lua
+++ lua/00_lazy.lua
@@ -2,21 +2,27 @@
 vim.g.mapleader = ","
 vim.mapleader = ","
 
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(plugins, opts)
+plugins = {
+	"folke/which-key.nvim",
+	{ "folke/neoconf.nvim", cmd = "Neoconf" },
+	"folke/neodev.nvim",
+}
+
+-- require("lazy").setup(plugins, opts)
+require("lazy").setup(plugins)
 
 -- Reload neovim whenever we save this file.
 -- vim.cmd([[
